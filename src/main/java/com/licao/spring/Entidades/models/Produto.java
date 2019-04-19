dd7b0cbd1d117a2e2cce2a7052bd8c72d94ff6cf
Vinicius Pereira Da Silva <viniciuspsilva1997@gmail.com>
2019-04-18 21:39:28 -0300

    Produto.class : adicionando lombook DataBuilder and Getter & Setter annotation

--- src/main/java/com/licao/spring/Entidades/models/Produto.java
+++ src/main/java/com/licao/spring/Entidades/models/Produto.java
@@ -12,10 +12,19 @@ import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+
+
 @Entity
 @JsonIdentityInfo(
 		  generator = ObjectIdGenerators.PropertyGenerator.class, 
 		  property = "id")
+@Builder
+@Getter
+@Setter
 public class Produto {
 
 	@Id
@@ -34,47 +43,6 @@ public class Produto {
 	private BigDecimal valor;
 	
 	@Column(nullable = false,columnDefinition = "boolean default false")
-	private boolean disponivel = false;
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
-	public BigDecimal getValor() {
-		return valor;
-	}
-
-	public void setValor(BigDecimal valor) {
-		this.valor = valor;
-	}
-
-	public boolean isDisponivel() {
-		return disponivel;
-	}
-
-	public void setDisponivel(boolean disponivel) {
-		this.disponivel = disponivel;
-	}
-	
+	private boolean disponivel;
 	
 }
